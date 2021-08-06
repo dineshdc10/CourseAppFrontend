c6e1916e236eda9f19e1a1efac072d88204a3481
Dinesh <cdinesh1083@gmail.com>
2021-08-06 17:19:15 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,13 +4,13 @@ function getConfig() {
     let config = {};
     if (process.env.NODE_ENV === 'production') {
         config = {
-            url: "https://course-app-backend.herokuapp.com",
+            url: "https://dc-courses.herokuapp.com",
             listurl: "https://s3-ap-southeast-1.amazonaws.com/he-public-data/courses26269ff.json"
         }
     }
     else {
         config = {
-            url: "https://course-app-backend.herokuapp.com",
+            url: "https://dc-courses.herokuapp.com",
             listurl: "https://s3-ap-southeast-1.amazonaws.com/he-public-data/courses26269ff.json"
         }
     }
